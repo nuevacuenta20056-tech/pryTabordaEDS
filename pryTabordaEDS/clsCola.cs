e42feb1eba0e1df95c076d7e64bac9420152ac49
nuevacuenta20056@gmail.com <nuevacuenta20055@gmail.com>
2026-04-16 12:13:34 -0300

    termine la clase cola

--- pryTabordaEDS/clsCola.cs
+++ pryTabordaEDS/clsCola.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,6 +35,18 @@ namespace pryTabordaEDS
             {
                 ult.Siguiente = nuevo;
                 ult = nuevo;
+            }
+
+        public void Eliminar()
+        {
+            if (pri == ult)
+            {
+                pri = null;
+                ult = null;
+            }
+            else
+            {
+                Primero = Primero.Siguiente;
             }
         }
     }
